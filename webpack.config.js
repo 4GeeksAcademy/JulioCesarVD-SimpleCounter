11d2bbd7b7660c7549451a3207033f5e3c07741f
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-05 10:49:22 +0000

    test

--- webpack.config.js
+++ webpack.config.js
@@ -3,65 +3,87 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 
 const port = 3000;
 let publicUrl = `ws://localhost:${port}/ws`;
-//only for gitpod
-if(process.env.GITPOD_WORKSPACE_URL){
+
+// Solo para gitpod
+if (process.env.GITPOD_WORKSPACE_URL) {
   const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
   publicUrl = `wss://${port}-${host}/ws`;
 }
-//only for codespaces
-if(process.env.CODESPACE_NAME){
+
+// Solo para codespaces
+if (process.env.CODESPACE_NAME) {
   publicUrl = `wss://${process.env.CODESPACE_NAME}-${port}.app.github.dev/ws`;
 }
 
 module.exports = {
-  entry: [
-    './src/js/index.js'
-  ],
+  entry: ['./src/js/index.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: 'style-loader', // Crea nodos de estilo a partir de cadenas de JS
+          },
+          {
+            loader: 'css-loader', // Traduce CSS a CommonJS
+          },
+        ],
+      },
+      // Para archivos mp3
+      {
+        test: /\.mp3$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/audio/[name][ext][query]',
         },
-        {
-          test: /\.(css)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        { 
-          test: /\.(png|svg|jpg|gif)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' } 
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
-    ]
+      },
+      // Para imágenes
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext][query]',
+        },
+      },
+      // Para fuentes
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext][query]',
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
     port,
     hot: true,
-    allowedHosts: "all",
+    allowedHosts: 'all',
     historyApiFallback: true,
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, 'dist'),
     },
     client: {
-      webSocketURL: publicUrl
+      webSocketURL: publicUrl,
     },
   },
   plugins: [
@@ -70,8 +92,12 @@ module.exports = {
     //   files: path.resolve(__dirname, "src"),
     // }),
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
-        template: 'template.html'
+      favicon: '4geeks.ico',
+      template: 'template.html',
     }),
-  ]
+    new HtmlWebpackTagsPlugin({
+      append: true,
+      tags: ['assets/audio/alarm_beep-clock-165474.mp3'], // Ruta al archivo mp3
+    }),
+  ],
 };
